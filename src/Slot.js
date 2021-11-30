381342566ba08fd7ebb06cb9d25f5495bd14406c
Barrett J Butterfield <gomowjames@users.noreply.github.com>
2021-11-30 12:13:06 -0800

    Change to props passed to component
    Slot will now be passed a color value, a function to be used onClick, and the position of the slot within the grid matrix.

--- src/Slot.js
+++ src/Slot.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-export default function Slot(props) {
-  return (
-    <button 
-      className={props.className} 
-//       onClick={() => props.onClick()}
-    >
-    </button>
-  )
+export default class Slot extends React.Component {
+  updateColor() {
+    
+    switch( this.props.color ) {
+      
+      case 1:
+        return <div className="slot color-1"></div>
+      
+      case 2:
+        return <div className="slot color-2"></div>
+      
+      default:
+        return <div className="slot transparent" onClick={() => this.props.handleDrop( this.props.position )}></div>
+
+    }
+  }
+
+  render() {
+    return this.updateColor();
+  }
 }
