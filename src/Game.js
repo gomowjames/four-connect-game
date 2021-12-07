12e7dcdf39c221eaafd8aa107aa5b1d89a5b08e2
Barrett J Butterfield <gomowjames@users.noreply.github.com>
2021-12-07 14:36:03 -0800

    Implement useState & useEffect to set grid size

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from './Grid.js';
 
 export default function Game() {
+
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: document.documentElement.clientWidth,
+    dynamicHeight: document.documentElement.clientHeight - 200
+  });
+
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: document.documentElement.clientWidth,
+      dynamicHeight: document.documentElement.clientHeight - 200
+    })
+  }
+  
+  useEffect(() => {
+    window.addEventListener('resize', setDimension);
+    
+    return(() => {
+        window.removeEventListener('resize', setDimension);
+    })
+  }, [screenSize])
+
   return (
     <section id="game">
-      <h1>Let's play!</h1>
-      <h2>Try to get four in a row horizontally, vertically, or diagonally.</h2>
-      
-      <Grid />
+      <Grid width={screenSize.dynamicWidth} height={screenSize.dynamicWidth}/>
     </section>
   )
 };
